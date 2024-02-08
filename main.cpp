958295dc5a9ab0a62c18e98a5f820313b4afeeb2
NoahV17 <nvario1@gmail.com>
2024-02-08 18:45:34 -0500

    almost all bugs fixed

--- main.cpp
+++ main.cpp
@@ -1,15 +1,14 @@
 #include "maze.h"
-#include "maze.cpp"
 
 int main(int argc, char* argv[]) {
             // Parse command-line args
     int seed = std::atoi(argv[1]);
-    int rows = std::atoi(argv[2]);
-    int cols = std::atoi(argv[3]);
-    //std::string fname = argv[4];
+    int height = std::atoi(argv[2]);
+    int width = std::atoi(argv[3]);
+    std::string fname = argv[4];
 
             // Create Maze object
-    Maze maze(seed, rows, cols);
+    Maze maze(seed, height, width, fname);
     maze.generateMaze();
     maze.PrintStuff();
     return 0;
